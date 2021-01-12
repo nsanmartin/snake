598965d7207177a7ce2c17b9b071f654a0959412
nsm <smnicolas@gmail.com>
2021-01-12 07:54:36 -0300

    Document the test

--- tests/box.cpp
+++ tests/box.cpp
@@ -1,7 +1,10 @@
 #include <World.hpp>
-
+#include <iostream>
 
 int main () {
+    std::cout << "Press `s' to start (the cube will start to move).\n"
+              << "Press `x', `X', `y', `Y', `z' or `Z' to change its direction.\n"
+              << "Press `f' or `b' to change the projection plane\n";
     World w{};
     w.Loop();
 }
